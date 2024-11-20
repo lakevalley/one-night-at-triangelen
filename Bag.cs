8a0fc2adc9818dee3e753f01899d74b37598201f
Malin W <malin.wiren@hotmail.com>
2024-11-20 11:04:25 +0100

    added bag class

---
+++ Bag.cs
@@ -0,0 +1,20 @@
+﻿namespace One_night_at_Triangelen;
+
+public class Bag:Location
+{
+    public Bag(Player player) : base(player)
+    {
+        Description = "";
+        _menu = new Menu(this);
+    }
+
+    public override void HandleResponse(string response)
+    {
+        switch (response)
+        {
+            case("return"):
+                new SecondFloor(_player);
+                break;
+        }
+    }
+}
