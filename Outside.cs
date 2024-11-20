a4491c855284b9f149c1a6e959835c490ab563dd
lakevalley <lakevalleymalmo@gmail.com>
2024-11-20 11:29:34 +0100

    added if statement

--- Outside.cs
+++ Outside.cs
@@ -1,29 +1,35 @@
 namespace One_night_at_Triangelen;
 
-public class Outside:Location
+public class Outside : Location
 {
-    public Outside(Player player):base(player)
+    public Outside(Player player) : base(player)
     {
-        Description = "You are outside. There is a bum there and he's wearing your jacket. There is an entrance to Triangelen and an entrance to the train station.";
+        Description =
+            "You are outside. There is a bum there and he's wearing your jacket. There is an entrance to Triangelen and an entrance to the train station.";
         _menu = new Menu(this);
     }
-    
+
     public override void HandleResponse(string response)
     {
         switch (response)
         {
-            case("enter triangelen"):
+            case ("enter triangelen"):
                 new FirstFloor(_player);
                 break;
-            case("enter station"):
+            case ("enter station"):
                 new Station(_player);
                 break;
-            case("steal jacket"):
-                Console.WriteLine("You shouldn't have done that. He is much stronger than you, in your poor state. He puts you under for at least 24 hours.");
+            case ("steal jacket"):
+                Console.WriteLine(
+                    "You shouldn't have done that. He is much stronger than you, in your poor state. He puts you under for at least 24 hours.");
                 break;
             case "trade":
-                _player.Inventory.Items.Remove("bottle");
-                _player.Inventory.Items.Add("jacket");
+                if (_player.Inventory.Items.Contains("bottle"))
+                {
+                    _player.Inventory.Items.Remove("bottle");
+                    _player.Inventory.Items.Add("jacket");
+                }
+                _player.Inventory.Print();
                 new Outside(_player);
                 break;
         }
