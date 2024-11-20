f4d650a20e03dc8fc7adbcfc3aa2a1751a27981a
plosgit <fadingcrap@gmail.com>
2024-11-20 11:42:51 +0100

    la till ny writeline efter man checkat pockets +var newresponse

--- ToiletStall.cs
+++ ToiletStall.cs
@@ -13,14 +13,20 @@ public class ToiletStall:Location
                 break;                   
             case("check pockets"):
                 _player.Inventory.Print();
-                new ToiletStall(_player);
+               // new ToiletStall(_player);
+                Console.WriteLine("What do you want to do next?");
+                var newResponse = Console.ReadLine();
+                if (newResponse is not null)
+                {
+                    HandleResponse(newResponse);
+                }
                 break;
         }                                                
     }                                                    
     
     public ToiletStall(Player player):base(player) // constructor
     {
-        Description = "You wake up in a toilet stall, but you have no memory of how you got here or where you are. There is a door.";
+        Description = "You wake up in a toilet stall, but you have no memory of how you got here or where you are. There is a door and theres something in your pocket.";
         _menu = new Menu(this);
     }
     
